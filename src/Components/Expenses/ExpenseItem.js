af3a59f2501151364eb60794f63c0754cdaa4f03
tharun1220 <tharun1220@gmail.com>
2024-02-03 16:04:09 +0530

    Manipulating the DOM-React states

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,20 +1,31 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log('Clicked!');
+    setTitle('Updated!');
+    setAmount(100);
+
+    console.log(title);
+    console.log(amount);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickHandler}>Change Price</button>
     </Card>
   );
 }
