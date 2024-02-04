16236aced0fb0537e6f12c1b008459381bfaeeee
tharun1220 <tharun1220@gmail.com>
2024-02-04 16:26:51 +0530

    Adding form inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import NewExpense from "./Components/NewExpense/NewExpense";
 import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
@@ -29,20 +31,20 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started"),
-    React.createElement(Expenses, {items: expenses})
-  );
-
-  // return (
-  //   <div>
-  //     <h2> Let's get started </h2>
-  //     <Expenses items={expenses} />
-
-  //   </div>
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started"),
+  //   React.createElement(Expenses, {items: expenses})
   // );
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+
+    </div>
+  );
 }
 
 export default App;
