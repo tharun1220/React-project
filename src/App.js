1547b4991a7c2103a125ba327c3fae5e09b1f6f3
tharun1220 <tharun1220@gmail.com>
2024-02-08 13:39:26 +0530

    Rendering expenses with perfomance optimizations

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./Components/NewExpense/NewExpense";
 import Expenses from "./Components/Expenses/Expenses";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.15,
-      date: new Date(2021, 3, 31),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: 184.45,
-      date: new Date(2021, 8, 11),
-    },
-    {
-      id: "e3",
-      title: "Food",
-      amount: 200.89,
-      date: new Date(2021, 7, 21),
-    },
-    {
-      id: "e4",
-      title: "Movies",
-      amount: 150.99,
-      date: new Date(2021, 11, 1),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.15,
+    date: new Date(2021, 3, 31),
+  },
+  {
+    id: "e2",
+    title: "Car Insurance",
+    amount: 184.45,
+    date: new Date(2021, 8, 11),
+  },
+  {
+    id: "e3",
+    title: "Food",
+    amount: 200.89,
+    date: new Date(2021, 7, 21),
+  },
+  {
+    id: "e4",
+    title: "Movies",
+    amount: 150.99,
+    date: new Date(2021, 11, 1),
+  },
+];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   // return React.createElement(
   //   "div",
@@ -47,7 +50,6 @@ function App() {
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
-
     </div>
   );
 }
